a1bc0684d5699e5f4ecebff8b0d7eff35bf0d7f4
Rafael Schmaiske de Morais <rafaelschmaiskedemorais@gmail.com>
2022-06-22 21:28:02 -0300

    Alteração no código da Vitória

--- tarefa-dia-3/pratica-3.js
+++ tarefa-dia-3/pratica-3.js
@@ -4,6 +4,6 @@ let tamanho_codigo = codigo.length
 
 if (tamanho_codigo > 6110){
     console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'(")
-} else if(6111 >= tamanho_codigo){
+} else if(6110 >= tamanho_codigo){
     console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D")
 }
