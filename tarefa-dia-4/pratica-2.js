f54a733ede35cf52034b48308fe7dfc20e270812
Rafael Schmaiske de Morais <rafaelschmaiskedemorais@gmail.com>
2022-06-24 19:00:14 -0300

    Desafio 2 dia 4

---
+++ tarefa-dia-4/pratica-2.js
@@ -0,0 +1,8 @@
+for (let contador = 1; contador <= 10; contador++) {
+    // escreva aqui um código para calcular exibir o resultado da tabuada do número 7. Ou seja, exibir o resultado de quanto é 7x1, 7x2, 7x3, 7x4, 7x5, etc
+    // veja que você pode utilizar o contador para fazer este cálculo.
+    let resultado = contador*7
+
+        console.log("O valor de 7x",contador,"é",resultado)
+
+}
